61dc9c32ba69d5100396bc1724dded936bc2fe57
Craig Melville <acekreations@gmail.com>
2018-10-04 22:40:28 -0700

    add Rank component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Stats from "../../components/Stats";
+import Rank from "../../components/Rank";
 
 class Home extends Component {
     render(){
         return (
+            <div>
             <Stats actions="104" rank="4" globalRank="1286"/>
+            <div className="uk-section uk-section-default uk-margin-large-top uk-padding-remove">
+                <Rank name="Denise Smith" activity="212" rank="1" profileImage="https://via.placeholder.com/64x64" />
+            </div>
+            </div>
         )
     }
 }
