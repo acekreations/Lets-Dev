219c3ece2292d48d8e890acd17177f1f721547bf
Craig Melville <acekreations@gmail.com>
2018-10-11 15:09:50 -0700

    add front end api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,30 @@
 import axios from "axios";
 
 export default {
-    //example
-    getFriends: function() {
-        return axios.get("/api/friends");
+    getUserProfile: function(id) {
+        return axios.get("/api/users/" + id);
+    },
+    displayFriends: function(id) {
+        return axios.get("/api/friends/" + id);
+    },
+    updateActivity: function(id) {
+        return axios.get("/api/update/" + id);
+    },
+    createRequest: function() {
+        //post obj to req.body. user id friend id
+        return axios.post("/api/request");
+    },
+    acceptRequest: function() {
+        //post obj to req.body. user id friend id
+        return axios.put("/api/request");
+    },
+    displayRequests: function(id) {
+        return axios.get("/api/request/" + id);
+    },
+    login: function(username) {
+        return axios.get("/api/login" + username);
+    },
+    signUp: function() {
+        return axios.post("/api/signup");
     }
 };
